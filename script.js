75889808ab43111c6a0505bec2c2124a2d25dbed
Frøydis Remme <64643539+fruremme@users.noreply.github.com>
2020-09-24 23:26:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// My first javascript assignment - MA1
+
+// task 1
+var firstVar;
+
+// task 2
+var name = "Frøydis";
+
+// task 3
+var number = 2020;
+
+// task 4 
+var division =(20/5);
+
+// task 5
+var frog = "padde";
+console.log(typeof frog);
+
+// task 6
+var orderHasShipped =true;
+
+// task 7 
+if (orderHasShipped === true) {
+    console.log("The order has been shipped");
+}
+
+else  {
+    console.log("we can`t find your order... it`s lost");
+}
+
+// task 8 
+ 
+for(var count = 0; count <= 9; count++) {
+    console.log(count);
+}
